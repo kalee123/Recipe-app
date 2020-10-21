eb83aeaed81cf863d9ac3e4f04ada083085c5196
kalee123 <jkaleedas@gmail.com>
2020-10-21 19:35:09 +0530

    Updates

---
+++ precache-manifest.8de7f2a748a397c4311bd9c42da8f65b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cab402612541242cdcecb520e3c59f9e",
+    "url": "/Recipe-App/index.html"
+  },
+  {
+    "revision": "bd627d91fdd1d4a8d8ea",
+    "url": "/Recipe-App/static/css/main.8960221e.chunk.css"
+  },
+  {
+    "revision": "2c4f81478335051d0701",
+    "url": "/Recipe-App/static/js/2.4e96db87.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Recipe-App/static/js/2.4e96db87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd627d91fdd1d4a8d8ea",
+    "url": "/Recipe-App/static/js/main.cc026070.chunk.js"
+  },
+  {
+    "revision": "c035ec1896e8fb26f1a6",
+    "url": "/Recipe-App/static/js/runtime-main.5105d1b0.js"
+  }
+]);
