789a895fa6c7dbab4ec5856d7b14769878f37ded
kalee123 <jkaleedas@gmail.com>
2020-10-21 19:42:28 +0530

    Your awesome message

---
+++ src/components/fetch.js
@@ -0,0 +1,18 @@
+
+
+export const fetchRecipes = async recipe => {
+    console.log("fetch");
+    const appId = "0a886643";
+    const key = "de87a8e0ff945e5d71564ed45bbac0f4";
+    const url = `https://api.edamam.com/search?q=${recipe}&app_id=${appId}&app_key=${key}&from=0&to=10`;
+    //Api call promise
+    try {
+      let response = await fetch(url);
+      let result = await response.json();
+      localStorage.setItem("recipes", JSON.stringify(result.hits));
+      return result.hits;
+    } catch (error) {
+      let response = [];
+      return response;
+    }
+  };
